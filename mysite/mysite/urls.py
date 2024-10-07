fa7d2d59461f11c04e3b453a3a8df3c09677beda
Jacqueline Ji <jackie@Jacquelines-MacBook-Pro.local>
2024-10-07 01:20:55 -0400

    Add new homepage

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
+    path('', views.homepage, name='homepage'),  # Homepage with links
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ mysite/mysite/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+def homepage(request):
+    return HttpResponse('''
+        <h1>Welcome to polls application!</h1>
+        <ul>
+            <li><a href="/polls/">Go to Polls</a></li>
+            <li><a href="/admin/">Go to Admin</a></li>
+        </ul>
+    ''')
